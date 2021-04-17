b09c69cb2317108ffaeb14389f43a8d48a859623
Maria Jaramillo <milagrosmjr20@gmail.com>
2021-04-17 11:16:18 -0400

    muestra2

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -1,16 +1,16 @@
 import React from "react"
-import twitter from "../../img/gorjeo 1.png"
-import instagram from "../../img/instagram 1.png"
-import linkedin from "../../img/linkedin (1) 1.png"
-import github from "../../img/logotipo-de-github 1.png"
+import twitter from "../../assets/img/gorjeo 1.png"
+import instagram from "../../assets/img/instagram 1.png"
+import linkedin from "../../assets/img/linkedin (1) 1.png"
+import github from "../../assets/img/logotipo-de-github 1.png"
 
 const SocialMedia = () => {
   return (
     <div className="social__media-group">
-      <img src={twitter} />
-      <img src={linkedin} />
-      <img src={instagram} />
-      <img src={github} />
+      <img alt="twitter logo" src={twitter} />
+      <img alt="linkedin logo" src={linkedin} />
+      <img alt="instagram logo" src={instagram} />
+      <img alt="github logo" src={github} />
     </div>
   )
 }
